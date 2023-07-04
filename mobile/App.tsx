b0729688a762f299a9c45d0b183d2e7a6ddc6e44
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2023-07-04 17:12:39 -0300

    feat: obetendo codigo github

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,7 @@
+import { useEffect } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 
 import {
   useFonts,
@@ -16,12 +18,43 @@ import { styled } from 'nativewind'
 
 const StyledStripes = styled(Stripes)
 
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/2c9f52e5d1239e24ef36',
+}
+
 export default function App() {
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const [request, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '2c9f52e5d1239e24ef36',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  useEffect(() => {
+    // console.log(
+    //   makeRedirectUri({
+    //     scheme: 'nlwspacetime',
+    //   }),
+    // )
+    if (response?.type === 'success') {
+      const { code } = response.params
+
+      console.log('Code ==>', code)
+    }
+  }, [response])
 
   if (!hasLoadedFonts) {
     return null
@@ -54,6 +87,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGithub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             COMEÇAR A CADASTRAR
