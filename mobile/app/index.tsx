62bf2e25a1878dc0a9caa63ec00a2ae2728f328d
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2023-07-04 17:49:09 -0300

    feat: navegando usuario, utilizando Expo Router

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import { useRouter } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
@@ -12,11 +13,11 @@ import {
 
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NlwLogo from './src/assets/nlw-spacetime-logo.svg'
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NlwLogo from '../src/assets/nlw-spacetime-logo.svg'
 import { styled } from 'nativewind'
-import { api } from './src/lib/api'
+import { api } from '../src/lib/api'
 
 const StyledStripes = styled(Stripes)
 
@@ -28,13 +29,15 @@ const discovery = {
 }
 
 export default function App() {
+  const router = useRouter()
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
 
-  const [request, response, signInWithGithub] = useAuthRequest(
+  const [, response, signInWithGithub] = useAuthRequest(
     {
       clientId: '2c9f52e5d1239e24ef36',
       scopes: ['identity'],
@@ -45,6 +48,18 @@ export default function App() {
     discovery,
   )
 
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const { token } = response.data
+
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+
   useEffect(() => {
     // console.log(
     //   makeRedirectUri({
@@ -53,19 +68,7 @@ export default function App() {
     // )
     if (response?.type === 'success') {
       const { code } = response.params
-
-      api
-        .post('/register', {
-          code,
-        })
-        .then((response) => {
-          const { token } = response.data
-
-          SecureStore.setItemAsync('token', token)
-        })
-        .catch((err) => {
-          console.log(err)
-        })
+      handleGithubOAuthCode(code)
     }
   }, [response])
 
